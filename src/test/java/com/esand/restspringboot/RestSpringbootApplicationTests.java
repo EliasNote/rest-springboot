7dd148754939d499b1b64190854dadf212e5fee9
Elias Mathias Sand <104523320+EliasNote@users.noreply.github.com>
2024-05-22 09:17:25 -0300

    Primeiro commit

---
+++ src/test/java/com/esand/restspringboot/RestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esand.restspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
